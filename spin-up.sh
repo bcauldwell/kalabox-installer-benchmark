c5ff6c9e5bbbad49be4c7dd1c8e8d9781e09b31b
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2014-10-10 16:44:39 -0700

    Adding script to benchmark time it takes to spin in boot2docker

---
+++ spin-up.sh
@@ -0,0 +1,20 @@
+#!/bash/bin
+
+echo '  : Spinning up boot2docker...'
+
+SCRIPT_DIR=$(pwd)
+B2D_DIR=$HOME/.boot2docker
+PROFILE_FILE='boot2docker.profile'
+
+if [ ! -d "$B2D_DIR" ]; then
+  mkdir -pv "$B2D_DIR"
+fi
+
+cp -fv $SCRIPT_DIR/$PROFILE_FILE $B2D_DIR
+mv -fv $B2D_DIR/$PROFILE_FILE $B2D_DIR/profile
+
+cd $HOME
+echo '  : Initilizing boot2docker...'
+boot2docker init
+echo '  : Starting boot2docker...'
+boot2docker up
